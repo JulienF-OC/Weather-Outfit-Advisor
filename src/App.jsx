ff667b43d5a2e5077d084d4e5344e841df8ff9f0
JulienF-OC <jucamlau16@gmail.com>
2026-03-12 13:13:36 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-blue-500 text-5xl font-bold">
+        Coucou
+      </h1>
+    </div>
+  )
+}
+
+export default App
